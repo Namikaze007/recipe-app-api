fb0c95e7ab5a2c9342fbf7649b919c645d057374
Pratyush sahoo <namikazesahoo3@gmail.com>
2022-11-01 12:58:51 +0530

    ACtions Added db

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,12 +3,10 @@ Calculator functions
 """
 
 def add(x, y):
-    """Add x and y """
+    """Add x and y."""
     return x + y
 
 
 def subtract(x, y):
-    """Subtract x from y and return result"""
-    return y - x
-
-    
+    """Subtract x from y and return result."""
+    return y - x   
